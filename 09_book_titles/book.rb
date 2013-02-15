fcf2f0d01cc198e70efc282a58fe5307ca26c058
PRUM Sambath <psb.sambath@gmail.com>
2013-02-15 15:26:52 +0700

    Enhanced 09_book_titles

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -1,14 +1,9 @@
-ARTICLES = %w[a an the]
-CONJUNCTIONS = %w[for and but or yet]
-PREPOSITIONS = %w[in of]
+SKIPPED_WORDS = %w[a an the and in of]
+
 class Book
-  def title=(title)
-    skipped_words = (ARTICLES << CONJUNCTIONS << PREPOSITIONS).flatten
-    @title = title.split.each { |t| t.capitalize! unless skipped_words.include?(t) }.join(' ')
-    @title = @title[0].capitalize << @title[1..-1]
-  end
+  attr_accessor :title
 
   def title
-    @title
+    @title.capitalize.gsub(/\w+/) { |w| SKIPPED_WORDS.include?(w) ? w : w.capitalize }
   end
 end
